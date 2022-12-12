a24991eac222f15cc8060de889de82dba41956cb
DESKTOP-2I0AT47\demon <dhowai.dev@gmail.com>
2022-12-12 16:35:43 +0000

    Use for flexible code for url instead of hardcoding

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import { useParams, Route, Link } from "react-router-dom";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
@@ -10,6 +10,7 @@ const DUMMY_DATA = [
 ];
 
 const QuoteDetail = () => {
+  const match = useRouteMatch();
   const params = useParams();
 
   const quote = DUMMY_DATA.find((quote) => quote.id === params.quoteId);
@@ -21,14 +22,14 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${params.quoteId}`} exact>
+      <Route path={match.path} exact>
         <div className="centered">
-          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+          <Link className="btn--flat" to={`${match.url}/comments`}>
             Load Comments
           </Link>
         </div>
       </Route>
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </Fragment>
